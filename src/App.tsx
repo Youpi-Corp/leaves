0dd183ffc1a3967cf22a468fa7b4c2c8693ffa23
ClementLeRigolo <clement1.thomas@epitech.eu>
2024-06-27 18:20:30 +0200

    [ADD]: Add button component and update header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Button from './components/interaction/button/Button'
 import Header from './components/layout/header/Header'
 
 function App() {
   return (
     <>
       <Header />
+      <Button style="primary">Primary</Button>
+      <Button style="secondary">Secondary</Button>
+      <Button style="tertiary">Tertiary</Button>
     </>
   )
 }
--- src/components/interaction/button/Button.tsx
+++ src/components/interaction/button/Button.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { FC, ReactNode } from 'react'
+import './Button.css'
 
-const Button: React.FC = () => {
-  return <button></button>
+interface ButtonProps {
+  style: 'primary' | 'secondary' | 'tertiary'
+  children: ReactNode
+  onClick?: () => void
+}
+
+const Button: FC<ButtonProps> = ({ style, children, onClick }) => {
+  return (
+    <button onClick={onClick} className={`button button-${style}`}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
