8e7bfc19e393200c5a8b68d3c87570acfde947d0
Ziiibz <corentin.wolff@epitech.eu>
2024-07-02 13:16:45 +0200

    [ADD] : Text widget

--- src/pages/poc/PagePoc.tsx
+++ src/pages/poc/PagePoc.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import WidgetCounter from '../../components/widget/WidgetCounter/front/widgetCounter'
 import Button from '../../components/interaction/button/Button'
+import TextWidget from '../../components/widget/WidgetCounter/front/widgetText'
 
 const PagePoc = () => {
   const [counters, setCounters] = useState<number[]>([])
@@ -15,6 +16,7 @@ const PagePoc = () => {
       {counters.map((counter) => (
         <WidgetCounter key={counter} />
       ))}
+      <TextWidget />
     </div>
   )
 }
