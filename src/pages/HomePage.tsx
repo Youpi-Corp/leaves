44753fd039f32e1d7c42b6a37f389c3aa74c7f1b
PumpKinzzzzz <jujulegeek2111@gmail.com>
2025-04-26 13:47:22 +0200

    ADD: Add Carousel to Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
+import CardCarousel from '../components/layout/CardCarousel'
 
 const HomePage: React.FC = () => {
+  const modules = [
+    { id: -1 },
+    { id: -1 },
+    { id: -1 },
+    { id: -1 },
+    { id: -1 },
+    { id: -1 },
+  ];
+
   return (
     <>
       <Header />
-      <div className="min-h-screen"></div>
+      <div className="min-h-screen">
+        <CardCarousel 
+          carouselId='featured'
+          itemsToShow={5} 
+        />
+
+        <CardCarousel 
+          carouselId='recent'
+          itemsToShow={5}
+        />
+
+        <CardCarousel 
+          carouselId='continue'
+          itemsToShow={5} 
+        />
+      </div>
       <Footer />
     </>
   )
