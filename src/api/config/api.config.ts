61164ff74656e388cb551376bef96549f45f7527
Bazouz660 <basile.trebus-hamann@epitech.eu>
2025-05-01 15:22:02 +0200

    [FIX]: change api url env varible name

--- src/api/config/api.config.ts
+++ src/api/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.API_BASE_URL || 'https://sap-m1i0.onrender.com',
+  BASE_URL:
+    import.meta.env.VITE_API_BASE_URL || 'https://sap-m1i0.onrender.com',
   ENDPOINTS: {
     AUTH: {
       LOGIN: '/auth/login',
