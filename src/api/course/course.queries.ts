cc6700ac918ca180b8d6e0206b0ce744431de179
PumpKinzzzzz <jujulegeek2111@gmail.com>
2025-05-15 10:33:51 +0200

    ADD: Api call for course save

---
+++ src/api/course/course.queries.ts
@@ -0,0 +1,23 @@
+import { API_CONFIG, getApiUrl } from '../config/api.config'
+
+export interface CourseCreateData {
+  name: string;
+  content: string;
+  module_id: number;
+  level: number;
+  public: boolean;
+}
+
+export const createCourseQuery = async (courseData: CourseCreateData) => {
+  const response = await fetch(getApiUrl('/course/create'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(courseData),
+  })
+  
+  if (!response.ok) throw new Error(response.status.toString())
+  return (await response.json()).data
+}
