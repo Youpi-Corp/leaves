fe28e3b5402816de60b565064fb4699cc6b5da89
Bazouz660 <basile.trebus-hamann@epitech.eu>
2025-06-23 20:36:30 +0200

    fix: Ensure secure attribute is set for cookies

--- src/helpers/cookies.utils.ts
+++ src/helpers/cookies.utils.ts
@@ -4,6 +4,7 @@ export const setCookie = (name: string, value: string, days: number = 14) => {
   Cookies.set(name, value, {
     expires: days,
     sameSite: 'none',
+    secure: true,
     path: '/',
   })
 }
